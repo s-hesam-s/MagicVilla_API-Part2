c7ecc7af37a71586f373d68077f5ad88f3f29aa6
s-hesam-s <hesamshoushtarian@gmail.com>
2024-03-07 13:43:43 +0330

    Section 5 - Add Refresh Token to Token Provider

--- MagicVilla_Utility/SD.cs
+++ MagicVilla_Utility/SD.cs
@@ -11,6 +11,8 @@
         }
 
         public static string AccessToken = "JWTToken";
+        public static string RefreshToken = "RefreshToken";
+
         public static string CurrentAPIVersion = "v2";
 
         public const string Admin = "admin";
